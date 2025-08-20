32fac15b021d238d6decd90277618d8aa0f89a13
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-08-20 20:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault(); // prevent form from refreshing page
+
+      // Get values from form fields
+      let firstName = document.queryselector("input[name='fname']".value;
+      let lastName = document.queryselector("input[name='lname')".value;
+
+      // Show full name in alert
+      alert(firstName + " " + lastName);
+	
+	
 
 }
